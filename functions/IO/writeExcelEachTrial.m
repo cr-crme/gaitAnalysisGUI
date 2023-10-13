7f81a70939f1d8f1b07b12d91e88335cb72e8bdb
Pariterre <pariterre@hotmail.com>
2023-10-13 11:47:07 -0400

    Added the capability to keep previous pass of exported trials

--- functions/IO/writeExcelEachTrial.m
+++ functions/IO/writeExcelEachTrial.m
@@ -24,29 +24,39 @@ function writeExcelEachTrial(c)
         'Param spatio-temporel Vitesse cycle (m/s) D'
         'Param spatio-temporel Vitesse cycle (m/s) G'
         'Param spatio-temporel Ratio marche (mm/pas/min) D'
-'Param spatio-temporel Ratio marche (mm/pas/min) G'
+        'Param spatio-temporel Ratio marche (mm/pas/min) G'
     };
 
+    oldData = [];
+    if isfile(filepath)
+        data = readtable(filepath);
+        oldData = data.Variables;
+    end
+
     outdata = array2table([
-        [c.resultsAll.kin.Right.pctToeOff];
-        [c.resultsAll.kin.Left.pctToeOff];
-        [c.resultsAll.kin.Right.pctContactTalOppose];
-        [c.resultsAll.kin.Left.pctContactTalOppose];
-        [c.resultsAll.kin.Right.pctToeOffOppose];
-        [c.resultsAll.kin.Left.pctToeOffOppose];
-        [c.resultsAll.kin.Right.pctSimpleAppuie];
-        [c.resultsAll.kin.Left.pctSimpleAppuie];
-        [c.resultsAll.kin.Right.distPas];
-        [c.resultsAll.kin.Left.distPas];
-        [c.resultsAll.kin.Right.distFoulee];
-        [c.resultsAll.kin.Left.distFoulee];
-        [c.resultsAll.kin.Right.tempsFoulee];
-        [c.resultsAll.kin.Left.tempsFoulee];
-        [c.resultsAll.kin.Right.vitFoulee];
-        [c.resultsAll.kin.Left.vitFoulee];
-        [c.resultsAll.kin.Right.distPas]./[c.resultsAll.kin.Right.vitCadencePasParMinute];
-        [c.resultsAll.kin.Left.distPas]./[c.resultsAll.kin.Left.vitCadencePasParMinute];
-    ]');
+        oldData;
+        [
+            [c.resultsAll.kin.Right.pctToeOff];
+            [c.resultsAll.kin.Left.pctToeOff];
+            [c.resultsAll.kin.Right.pctContactTalOppose];
+            [c.resultsAll.kin.Left.pctContactTalOppose];
+            [c.resultsAll.kin.Right.pctToeOffOppose];
+            [c.resultsAll.kin.Left.pctToeOffOppose];
+            [c.resultsAll.kin.Right.pctSimpleAppuie];
+            [c.resultsAll.kin.Left.pctSimpleAppuie];
+            [c.resultsAll.kin.Right.distPas];
+            [c.resultsAll.kin.Left.distPas];
+            [c.resultsAll.kin.Right.distFoulee];
+            [c.resultsAll.kin.Left.distFoulee];
+            [c.resultsAll.kin.Right.tempsFoulee];
+            [c.resultsAll.kin.Left.tempsFoulee];
+            [c.resultsAll.kin.Right.vitFoulee];
+            [c.resultsAll.kin.Left.vitFoulee];
+            [c.resultsAll.kin.Right.distPas]./[c.resultsAll.kin.Right.vitCadencePasParMinute];
+            [c.resultsAll.kin.Left.distPas]./[c.resultsAll.kin.Left.vitCadencePasParMinute];
+        ]';
+    ]);
+    
     outdata.Properties.VariableNames = header;
     writetable(outdata, filepath, 'Delimiter', sep);
 
